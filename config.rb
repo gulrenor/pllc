f699ba44130acc58edccf7c22586f9881a814b6d
gulrenor <gulrenor@gmail.com>
2016-11-21 12:28:39 -0500

    Added Compass

---
+++ config.rb
@@ -0,0 +1,12 @@
+css_dir = "themes/lc/static/css"
+sass_dir = "themes/lc/static/sass"
+images_dir = "static/images"
+
+# Swap uncomment for live
+# output_style = :compressed
+output_style = :expanded
+
+# Debug
+line_comments = true
+
+preferred_syntax = :scss
