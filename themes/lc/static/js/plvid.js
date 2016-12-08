54cd9b82c7756e52c670138029690cac82feef06
gulrenor <gulrenor@gmail.com>
2016-12-08 12:05:25 -0500

    Fixes #4, Fixes #6

--- themes/lc/static/js/plvid.js
+++ themes/lc/static/js/plvid.js
@@ -1,8 +1,47 @@
+// plvid.js
 // Shows controls for HTML video player when hovered
+
 $(document).ready(function() {
+  // Which breakpoint is the mobile menu?
+  var mobileNavBreakpoint = 'small';
+  
+  function isMobileNav() {
+    // Check the html body::before which contains
+    // the current breakpoint
+    var breakpoint = window.getComputedStyle($('body')[0], ':before').getPropertyValue('content').replace(/\"/g, '');
+    if (breakpoint === mobileNavBreakpoint) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
+  // FIXME: This should all probably be refactored...
+  
+  // Always show controls when mobile size
+  // Check again when page is resized
+  if (isMobileNav()) {
+    $('video').prop('controls', true);  
+  };
+    
+  $(window).resize(function() {
+    if (isMobileNav()) {
+      $('video').prop('controls', true);  
+    } else {
+      $('video').prop('controls', false);  
+    };
+  });
+
   $('video').hover(
     function() {
-      $(this).prop('controls', !$(this).prop('controls'));
+      $(this).prop('controls', true);
+    },
+    function() {
+      if (!isMobileNav()) {
+        $(this).prop('controls', false);  
+      }
     }
+    //function() {$(this).prop('controls', !$(this).prop('controls'));}
   );
+  
 });
